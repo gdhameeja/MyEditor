329b31c1147545d9376db6e889a9174fc1357d65
gdhameeja <gaurav@melvault.com>
2019-06-27 13:38:30 +0530

    Enable raw mode while editing and disable raw mode at exit

--- kilo.c
+++ kilo.c
@@ -1,7 +1,41 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+/* Turning echo off (terminal raw mode)
+ * don't print to terminal what I type
+ * terminal attrs read into termios struct
+ * by tcgetattr()
+ * modify the attributes and re-apply then using
+ * tcsetattr(). TCSAFLUSH tells when to apply the change
+ */
+
+struct termios orig_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  // ECHO is a bitflag defined as 0100.
+  // ~ (NOT) reverses the bits to 1011
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
-  printf("Hello, World!\n");
+  enableRawMode();
+  
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1) {
+    if (c == 'q') {
+      break;
+    }
+  }
   return 0;
 }
+  
